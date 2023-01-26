a2e5f4591095f938d8035f77e92cef192a222d74
AgentHumer <agent_hammer@mail.ru>
2023-01-26 23:36:23 +0500

    Создан container

--- src/pages/ResultPage/ResultPage.js
+++ src/pages/ResultPage/ResultPage.js
@@ -1,7 +1,9 @@
+import style from "../ResultPage/ResultPage.module.css";
+
 export default function ResultPage()
 {
     return(
-        <div>
+        <div className={style.container}>
 
         </div>
     )
