aa56a8472e351e861daab84fd60ae0df182371b7
Black Soul <mr.asaglaev@mail.ru>
2023-01-26 22:08:32 +0500

    [Update]: Удалены лишние пробелы, убран массив questions и другое

--- src/pages/TestPage/TestPage.js
+++ src/pages/TestPage/TestPage.js
@@ -1,126 +1,60 @@
-
 import style from "./TestPage.module.css"
 import {useState} from "react";
-
+import useApi from "../../api/useApi";
 
 export default function  TestPage()
 {
     const [count, setCount] = useState(1);
+    const [score, setScore] = useState(0);
 
-    const [score,setScore ] = useState(0);
+    const [currentResponse, setCurrentResponse] = useState(null)
 
-    const questions = [
+    const {getQuestionsData} = useApi()
 
-        {
-            id:1,
-            img:"1.jpg",
-            question:"Ты любишь Чон Гука?",
-            answers:[
-                {
-                    id: 1,
-                    text: "Да"
-                },
-                {
-                    id: 2,
-                    text: "Да"
-                },
-                {
-                    id: 3,
-                    text: "ДА,АБСОЛЮТНО"
-                }
-            ],
-            correctAnswer:3,
-        },
+    const questions = getQuestionsData()
 
-        {
-            id:1,
-            img:"2.jpg",
-            question:"Ты любишь Чон Гука?",
-            answers:[
-                {
-                    id: 1,
-                    text: "yes"
-                },
-                {
-                    id: 2,
-                    text: "YES!"
-                },
-                {
-                    id: 3,
-                    text: "ДА,АБСОЛЮТНО"
-                }
-            ],
-            correctAnswer:3,
-        },
-
-        {
-            id:1,
-            img:"3.jpg",
-            question:"Ты любишь Чон Гука?",
-            answers:[
-                {
-                    id: 1,
-                    text: "Да"
-                },
-                {
-                    id: 2,
-                    text: "Да"
-                },
-                {
-                    id: 3,
-                    text: "ДА,АБСОЛЮТНО"
-                }
-            ],
-            correctAnswer:3,
+    const onNextClickHandler = () => {
+        if (currentResponse != null) {
+            if (currentResponse === questions[count - 1].correctAnswer)
+                setScore(score + 1)
         }
-    ]
+        setCurrentResponse(null)
+        setCount(count + 1)
+    }
 
+    const onRadioClickHandler = (ctx) => {
+        const answerId = ctx.target.dataset.id
+        setCurrentResponse(answerId)
+    }
 
-
-    return(
+    return (
         <div className={style.container}>
-
-            <div className={style.img} style={{backgroundImage:`url(img/${questions[count - 1].img})`}}></div>
-
+            <div
+                className={style.img}
+                style={{backgroundImage:`url(img/${questions[count - 1].img})`}}
+            >
+            </div>
             <div className={style.question}>
-                <div className={style.question__caption}>{questions[count - 1].question}</div>
+                <div className={style.question__caption}>
+                    {questions[count - 1].question}
+                </div>
                 <div className={style.question__answers}>
-                            {questions[count - 1].answers.map(question => (
-                                question.id === questions[count - 1].correctAnswer ? (
-
-                                    <div className={style.question__answer}>
-                                        <input type={"radio"} name={count}/>
-
-                                        <p>{question.text}</p>
-
-                                    </div>
-                                    )
-                                    : (
-
-                                    <div className={style.question__answer}>
-                                            <input type={"radio"} name={count} />
-
-                                            <p>{question.text}</p>
-
-                                    </div>
-                                    )
-                            ))}
+                    {questions[count - 1].answers.map(answer => (
+                        <div className={style.question__answer}>
+                            <input
+                                type={"radio"}
+                                data-id={answer.id}
+                                name={count}
+                                onClick={onRadioClickHandler}
+                            />
+                            <p>{answer.text}</p>
+                        </div>
+                    ))}
                 </div>
             </div>
-
             <div className={style.counter__container}>
                 <button className={style.button}
-                        onClick={() =>{
-                            setCount(count + 1)
-                            if (count >= questions.length){
-                                setCount(1);
-                            }
-
-
-                        }
-
-                        }
-                    >Дальше
+                        onClick={onNextClickHandler}>Дальше
                 </button>
                 <div className={style.counter}>{count}/{questions.length}</div>
             </div>
