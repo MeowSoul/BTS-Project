ce2b46ffa2cd4556259d7f7ef8e2f2576ec6a601
AgentHumer <agent_hammer@mail.ru>
2023-01-26 23:34:53 +0500

    Убраны лишние пробелы

--- src/componets/header/header.js
+++ src/componets/header/header.js
@@ -1,14 +1,11 @@
-
 import style from "./header.module.css"
-
-
 
 export default function Header()
 {
     return(
 
         <header className={style.header}>
-            <div className={style.logo} > <img src={"img/logo.png"}/> </div>
+            <div className={style.logo} > <img src={"img/logo.png"} alt={"https://github.com/"}/> </div>
             <div className={style.tests}>Тесты</div>
             <div className={style.test__name}>Насколько ты знаешь BTS?</div>
         </header>
