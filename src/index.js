c98ea4a188f2b337f41c9557a190154fb84c2323
AgentHumer <agent_hammer@mail.ru>
2023-01-30 23:10:20 +0500

    ...

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
+import ScoreContextProvider, {ScoreContext} from "./context/ScoreContext";
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>
+      <ScoreContextProvider>
+          <BrowserRouter>
+              <App />
+          </BrowserRouter>
+      </ScoreContextProvider>
   </React.StrictMode>
 );
 
