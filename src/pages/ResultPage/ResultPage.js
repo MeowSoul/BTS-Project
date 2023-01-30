496c73b482574997ba69bca0360ab02a9ad852fa
AgentHumer <agent_hammer@mail.ru>
2023-01-30 23:08:26 +0500

    На странце с результом выведена картинка затычка

--- src/pages/ResultPage/ResultPage.js
+++ src/pages/ResultPage/ResultPage.js
@@ -1,10 +1,21 @@
 import style from "../ResultPage/ResultPage.module.css";
+import {useContext} from "react";
+import {ScoreContext} from "../../context/ScoreContext";
+
+
+
 
 export default function ResultPage()
 {
+    const {FinalScore, setFinalScore} = useContext(ScoreContext)
+
+    console.log(FinalScore)
+
     return(
         <div className={style.container}>
-
+            <div className={style.image_block}>
+                <img src={"/img/0-24.jpg"} alt={"Google.com"} ></img>
+            </div>
         </div>
     )
 }
