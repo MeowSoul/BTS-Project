dd873af1053b20b40918b279afe53b52dde0c18e
Black Soul <mr.asaglaev@mail.ru>
2023-02-25 13:07:19 +0500

    Убраны лишние пробелы

--- src/componets/header/header.js
+++ src/componets/header/header.js
@@ -3,14 +3,10 @@ import style from "./header.module.css"
 export default function Header()
 {
     return(
-
         <header className={style.header}>
             <div className={style.logo} > <img src={"img/logo.png"} alt={"https://github.com/"}/> </div>
             <div className={style.tests}>Тесты</div>
             <div className={style.test__name}>Насколько ты знаешь BTS?</div>
         </header>
-
     )
-
-
 }
