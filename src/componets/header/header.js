9b6f053818da34c56ecf74eea2b02331364e47c1
AgentHumer <agent_hammer@mail.ru>
2023-03-09 21:33:48 +0500

    Убрано лого

--- src/componets/header/header.js
+++ src/componets/header/header.js
@@ -4,7 +4,6 @@ export default function Header()
 {
     return(
         <header className={style.header}>
-            <div className={style.logo} > <img src={"img/logo.png"} alt={"https://github.com/"}/> </div>
             <div className={style.tests}>Тесты</div>
             <div className={style.test__name}>Насколько ты знаешь BTS?</div>
         </header>
